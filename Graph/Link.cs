f6bfd94ed4578dad57497b9faca984c9e1261d84
julio <julio@DESKTOP-QTDGH0L>
2021-07-16 08:18:11 -0300

    Adicionar arquivos de projeto.

---
+++ Graph/Link.cs
@@ -0,0 +1,26 @@
+﻿namespace Graph
+{
+    public interface ILink<T>
+    {
+        T Source { get; }
+        T Target { get; }
+    }
+
+    public class Link<T> : ILink<T>
+    {
+        public Link(T source, T target)
+        {
+            Source = source;
+            Target = target;
+        }
+
+        public T Source { get; private set; }
+        public T Target { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Source?.ToString()} -> {Target?.ToString()}";
+        }
+
+    }
+}
---
+++ SubtitleTimeshift/Shifter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SubtitleTimeshift
+{
+    public class Shifter
+    {
+        async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
+        {
+            byte[] bytes = null;
+            ArrayList dates = new ArrayList();
+            string dtReplace;
+            int i = 0;
+
+            string linha = null;
+            var reader = new StreamReader(input, encoding, true);
+
+            while (null != (linha = await reader.ReadLineAsync()))
+            {
+                var rx = new Regex(@"\d{2}:\d{2}:\d{2},\d{3}");
+                MatchCollection matches = rx.Matches(linha);
+
+                if(matches.Count > 0)
+                {
+                    foreach (Match match in rx.Matches(linha))
+                    {
+                        dtReplace = match.Value.Replace(",", ".");
+                        dates.Add(dtReplace);
+                    }
+
+                    DateTime dt1 = DateTime.ParseExact(dates[i].ToString(), "hh:mm:ss.fff", null);
+                    DateTime dt2 = DateTime.ParseExact(dates[i + 1].ToString(), "hh:mm:ss.fff", null);
+
+                    bytes = Encoding.UTF8.GetBytes(dt1.Add(timeSpan).ToString("HH:mm:ss.fff").Replace(".", ",") + " --> " + dt2.Add(timeSpan).ToString("HH:mm:ss.fff").Replace(".", ",") + "\n");
+                    i = i+2;
+                }
+                else
+                {
+                    bytes = Encoding.UTF8.GetBytes(linha + "\n");
+                }
+
+                output.Write(bytes, 0, bytes.Length);
+
+            }
+
+
+        }
+    }
+}
